f5e197d4db5063f3a6830713fa391516f3b456fb
Damjan Trifunovic <damjan.trifunovic@gmail.com>
2018-12-24 19:22:26 +0100

    begin DTOs

---
+++ planit-teams/planit-data/DTOs/CommentDTO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using planit_data.Entities;
+
+namespace planit_data.DTOs
+{
+    public class CreateCommentDTO
+    {
+        public String Text { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public int CardId { get; set; }
+        public int UserId { get; set; }
+    }
+
+    public class ReadCommentDTO
+    {
+        public int CommentId { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public Card Card { get; set; }
+        public User User { get; set; }
+
+        public ReadCommentDTO (Comment comment)
+        {
+            CommentId = comment.CommentId;
+            TimeStamp = comment.TimeStamp;
+            Card = comment.Card;
+            User = comment.User;
+        }
+    }
+
+    public class UpdateCommentDTO
+    {
+        public int CommentId { get; set; }
+        public String Text { get; set; }
+    }
+
+    public class DeleteCommentDTO
+    {
+        public int CommentId { get; set; }
+    }
+}
---
+++ planit-teams/planit-data/DTOs/UserDTO.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using planit_data.Entities;
+
+namespace planit_data.DTOs
+{
+
+    public class CreateUserDTO
+    {
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+    }
+
+    public class ReadUserDTO
+    {
+        public int UserID { get; set; }
+        public String Username { get; set; }
+        public String Email { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public List<Notification> Notifications { get; set; }
+        public List<Card> Cards { get; set; }
+        public List<Card> WatchedCards { get; set; }
+        public List<Permission> Permissions { get; set; }
+
+        public ReadUserDTO (User user)
+        {
+            UserID = user.UserId;
+            Username = user.IdentificationUser.UserName;
+            Email = user.IdentificationUser.Email;
+            FirstName = user.FirstName;
+            LastName = user.LastName;
+
+        }
+    }
+
+    public class UpdateUserDTO
+    {
+        public int UserID { get; set; }
+        public String Username { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public List<Notification> Notifications { get; set; }
+        public List<Card> Cards { get; set; }
+        public List<Card> WatchedCards { get; set; }
+        public List<Permission> Permissions { get; set; }
+    }
+
+    public class DeleteUserDTO
+    {
+        public int UserID { get; set; }
+    }
+
+}
