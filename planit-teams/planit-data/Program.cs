ad7c7e462d7d315834a853653e9cfa9aabd11baa
LastChanceKatze <last.chance.katze@elfak.rs>
2018-12-24 19:23:22 +0100

    Repozitorijumi

--- planit-teams/planit-data/Program.cs
+++ planit-teams/planit-data/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using planit_data.Entities;
+using planit_data.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,70 +14,86 @@ namespace planit_data
     {
         static void Main(string[] args)
         {
-            ApplicationContext context = new ApplicationContext();
+            //ApplicationContext context = new ApplicationContext();
 
-            //User Ana = new User()
+            ////User Ana = new User()
+            ////{
+            ////    FirstName = "novaAna",
+            ////    LastName = "novaAna"
+            ////};
+
+            ////context.UserList.Add(Ana);
+            ////context.SaveChanges();
+            ////int a = 5;
+            ////var store = new UserStore<ApplicationUser>(context);
+            ////var manager = new UserManager<ApplicationUser>(store);
+
+            ////var user = new ApplicationUser() { Email = "ana@stojanovic", UserName = "ana@stojanovic" };
+            ////var usmanger = manager.Create(user, "Ana123!");
+
+            ////User newUser = new User()
+            ////{
+            ////    FirstName = "Damjan",
+            ////    LastName = "Trifunovic",
+            ////    IdentificationUser = user
+            ////};
+
+            ////context.UserList.Add(newUser);
+            ////context.SaveChanges();
+
+            ////User Damjan = context.UserList.Where(x => x.UserId == 2).FirstOrDefault<User>();
+            ////Board b = new Board()
+            ////{
+            ////    Name = "Prvi board"
+            ////};
+
+            ////Permission p = new Permission()
+            ////{
+            ////    IsAdmin = true,
+            ////    Board = b,
+            ////    User = Damjan
+            ////};
+
+            ////context.Permissions.Add(p);
+            ////context.SaveChanges();
+
+            ////CardList list = new CardList()
+            ////{
+            ////    Name = "To do",
+            ////    Color ="Blue"
+            ////};
+
+            ////list.Board = context.Boards.Where(x => x.BoardId == 1).FirstOrDefault();
+            ////context.CardLists.Add(list);
+            ////context.SaveChanges();
+            //ApplicationContext newContext = new ApplicationContext();
+
+            //CardList list = context.CardLists.Where(x => x.ListId == 1).First();
+            //Card card = new Card()
             //{
-            //    FirstName = "novaAna",
-            //    LastName = "novaAna"
+            //    Name = "Nova nova kartica"
             //};
 
-            //context.UserList.Add(Ana);
+            //list.Cards.Add(card);
+
+            //context.Entry(list).State = System.Data.Entity.EntityState.Modified;
             //context.SaveChanges();
-            //int a = 5;
-            //var store = new UserStore<ApplicationUser>(context);
-            //var manager = new UserManager<ApplicationUser>(store);
 
-            //var user = new ApplicationUser() { Email = "ana@stojanovic", UserName = "ana@stojanovic" };
-            //var usmanger = manager.Create(user, "Ana123!");
 
-            //User newUser = new User()
-            //{
-            //    FirstName = "Damjan",
-            //    LastName = "Trifunovic",
-            //    IdentificationUser = user
-            //};
-
-            //context.UserList.Add(newUser);
-            //context.SaveChanges();
-
-            //User Damjan = context.UserList.Where(x => x.UserId == 2).FirstOrDefault<User>();
-            //Board b = new Board()
-            //{
-            //    Name = "Prvi board"
-            //};
-
-            //Permission p = new Permission()
-            //{
-            //    IsAdmin = true,
-            //    Board = b,
-            //    User = Damjan
-            //};
-
-            //context.Permissions.Add(p);
-            //context.SaveChanges();
-
-            //CardList list = new CardList()
-            //{
-            //    Name = "To do",
-            //    Color ="Blue"
-            //};
-
-            //list.Board = context.Boards.Where(x => x.BoardId == 1).FirstOrDefault();
-            //context.CardLists.Add(list);
-            //context.SaveChanges();
-
-            CardList list = context.CardLists.Where(x => x.ListId == 1).First();
-            Card card = new Card()
+            //List<Card> cards = newContext.Cards.ToList();
+            Card c;
+            using (UnitOfWork unit = new UnitOfWork())
             {
-                Name = "Kartice"
-            };
-
-            list.Cards.Add(card);
-
-            context.Entry(list).State = System.Data.Entity.EntityState.Modified;
-            context.SaveChanges();
-                
+                c = unit.CardRepository.GetById(3);
+                Comment com = new Comment()
+                {
+                    Text = "Novi komentar",
+                    Card = c
+                };
+                unit.CommentRepository.Insert(com);
+                unit.Save();
+            }
+            int a = 4;
 
         }
     }
---
+++ planit-teams/planit-data/Repository/UnitOfWork.cs
@@ -0,0 +1,113 @@
+﻿using planit_data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace planit_data.Repository
+{
+    public class UnitOfWork : IDisposable
+    {
+        private ApplicationContext context = new ApplicationContext();
+        private bool disposed = false;
+
+        private GenericRepository<User> userRepository;
+        private GenericRepository<Board> boardRepository;
+        private GenericRepository<Card> cardRepository;
+        private GenericRepository<CardList> cardListRepository;
+        private GenericRepository<Comment> commentRepository;
+        private GenericRepository<Notification> notificationRepository;
+        private GenericRepository<Permission> permissionRepository;
+
+        #region Property 
+        public GenericRepository<User> UserRepository
+        {
+            get
+            {
+                if (userRepository == null)
+                    userRepository = new GenericRepository<User>(context);
+                return userRepository;
+            }
+        }
+        public GenericRepository<Board> BoardRepository
+        {
+            get
+            {
+                if (boardRepository == null)
+                    boardRepository = new GenericRepository<Board>(context);
+                return boardRepository;
+            }
+        }
+        public GenericRepository<Card> CardRepository
+        {
+            get
+            {
+                if (cardRepository == null)
+                    cardRepository = new GenericRepository<Card>(context);
+                return cardRepository;
+            }
+        }
+        public GenericRepository<CardList> CardListRepository
+        {
+            get
+            {
+                if (cardListRepository == null)
+                    cardListRepository = new GenericRepository<CardList>(context);
+                return cardListRepository;
+            }
+        }
+        public GenericRepository<Comment> CommentRepository
+        {
+            get
+            {
+                if (commentRepository == null)
+                    commentRepository = new GenericRepository<Comment>(context);
+                return commentRepository;
+            }
+        }
+        public GenericRepository<Notification> NotificationRepository
+        {
+            get
+            {
+                if (notificationRepository == null)
+                    notificationRepository = new GenericRepository<Notification>(context);
+                return notificationRepository;
+            }
+        }
+        public GenericRepository<Permission> PermissionRepository
+        {
+            get
+            {
+                if (permissionRepository == null)
+                    permissionRepository = new GenericRepository<Permission>(context);
+                return permissionRepository;
+            }
+        }
+        #endregion
+
+        public bool Save()
+        {
+            try
+            {
+                context.SaveChanges();
+                return true;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+        }
+
+        public void Dispose()
+        {
+            if(!this.disposed)
+            {
+                context.Dispose();
+            }
+            this.disposed = true;
+            GC.SuppressFinalize(this);
+        }
+    }
+}
