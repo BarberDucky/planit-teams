cd5033a887f17198a7e33b4b08630f5e8e1131c3
Damjan Trifunovic <damjan.trifunovic@gmail.com>
2019-04-02 14:12:12 +0200

    move wip

--- planit-teams/planit-client-wpf/View/CardListView.xaml.cs
+++ planit-teams/planit-client-wpf/View/CardListView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using planit_client_wpf.Base;
 using planit_client_wpf.ViewModel;
+using planit_client_wpf.Model;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -38,6 +39,23 @@ namespace planit_client_wpf.View
             {
                 var vm = DataContext as CardListViewModel;
                 vm.MessageBoxRequest += MessageBoxRequest;
+            }
+        }
+
+        private void ListBox_Drop(object sender, DragEventArgs e)
+        {
+            MessageBox.Show(e.Data.GetData(DataFormats.Text).ToString());
+        }
+
+        private void listbox_MouseMove(object sender, MouseEventArgs e)
+        {
+            base.OnMouseMove(e);
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                DataObject data = new DataObject();
+                data.SetData(DataFormats.StringFormat, ((ReadCard)(((ListBox)sender).SelectedItem)).CardId);
+
+                DragDrop.DoDragDrop(this, data, DragDropEffects.Move);
             }
         }
     }
